560194108f31904bf0ea3554e85b2015e54f6719
unknown <snaiderquispe603@gmail.com>
2025-11-28 11:21:46 -0400

    XD

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,14 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App.jsx'
 import './index.css'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000,
+      retry: 2,
+    },
+  },
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
